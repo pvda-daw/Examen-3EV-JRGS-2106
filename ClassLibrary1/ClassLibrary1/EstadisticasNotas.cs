b6e4dff1e6e1b2a06dd1557b16d845710292970f
Laila <laila.pc@gmail.com>
2021-06-08 20:24:43 +0200

    Cambios para el examen realizados.

---
+++ ClassLibrary1/ClassLibrary1/EstadisticasNotas.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Examen3EV_NS
+{
+    /// <summary>
+    /// Esta clase nos calcula las estadísticas de un conjunto de notas.
+    /// </summary>
+    public class EstadisticasNotas 
+    {
+        /// <summary>
+        /// Numero de suspensos.
+        /// </summary>
+        private int suspensos;
+
+        /// <summary>
+        /// Número de aprobados.
+        /// </summary>
+        private int aprobados;
+
+        /// <summary>
+        /// Número de notables.
+        /// </summary>
+        private int notables;
+
+        /// <summary>
+        /// Número de sobresalientes.
+        /// </summary>
+        private int sobresalientes;
+
+        private double notaMedia;
+
+        public int Suspensos { get => suspensos; set => suspensos = value; }
+        public int Aprobados { get => aprobados; set => aprobados = value; }
+        public int Notables { get => notables; set => notables = value; }
+        public int Sobresalientes { get => sobresalientes; set => sobresalientes = value; }
+        public double NotaMedia { get => notaMedia; set => notaMedia = value; }
+
+        /// <summary>
+        /// Constructor por defecto.
+        /// </summary>
+        /// <remarks>
+        /// Inicializa todos los valores a 0.
+        /// </remarks>
+        public EstadisticasNotas()
+        {
+            suspensos = 0;
+            aprobados = 0;
+            notables = 0;
+            sobresalientes = 0;
+            notaMedia = 0.0;
+        }
+
+        /// <summary>
+        /// Constructor sobrecargado. Calcula las estadísticas al crear el objeto a partir de <paramref name="listaNotas"/>.
+        /// </summary>
+        /// <param name="listaNotas">Lista de notas.</param>
+        /// <remarks>Utiliza el método <see cref="CalcularEstadisticas(List{int})"/> para generar las estadísticas.</remarks>
+        public EstadisticasNotas(List<int> listaNotas)
+        {
+            notaMedia = CalcularEstadisticas(listaNotas);
+        }
+
+        /// <summary>
+        /// <para>Para un conjunto de <paramref name="listaNotas"/>, calculamos las estadisticas.</para>
+        /// <para>calcula la media y el número de suspensos/aprobados/notables/sobresalientes.</para>
+        /// </summary>
+        /// <remarks>En los casos en los que se introduzcan listas vacias o elementos inválidos devoverá <see cref="Exception"/>.</remarks>
+        /// <param name="listaNotas">Lista de notas</param>
+        /// <returns>Media total de las notas.</returns>
+        public double CalcularEstadisticas(List<int> listaNotas)
+        {                                 
+             // Si la lista no contiene elementos, devolvemos un error
+            if (listaNotas.Count <= 0) {
+                throw new Exception("Lista vacia");
+            }
+
+            for (int i = 0; i < listaNotas.Count; i++) {
+                // comprobamos que las notas están entre 0 y 10 (mínimo y máximo), si no, error
+                if (listaNotas[i] < 0 || listaNotas[i] > 10)
+                {
+                    throw new Exception("Nota invalida");
+                }
+            }
+
+            for (int i = 0; i < listaNotas.Count; i++)
+            {
+                if (listaNotas[i] < 5)
+                {
+                    // Por debajo de 5 suspenso
+                    suspensos++;
+                }
+                else if (listaNotas[i] >= 5 && listaNotas[i] < 7)
+                {
+                    // Nota para aprobar: 5
+                    aprobados++;
+                }
+                else if (listaNotas[i] >= 7 && listaNotas[i] < 9)
+                {
+                    // Nota para notable: 7
+                    notables++;
+                }
+                else if (listaNotas[i] >= 9)
+                {
+                    // Nota para sobresaliente: 9
+                    sobresalientes++;
+                }
+
+                notaMedia = notaMedia + listaNotas[i];
+            }
+
+            notaMedia = notaMedia / listaNotas.Count;
+
+            return notaMedia;
+        }
+    }
+}
--- ClassLibrary1/UnitTestProject1/UnitTest1.cs
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Examen3EV_NS;
+using System;
 
 namespace UnitTestProject1
 {
@@ -8,23 +9,184 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalcularEstadisticas_ListaConValoresValidos_ActualizaNotas()
         {
+            // Preparación de la prueba
             List<int> notas = new List<int>();
-
             notas.Add(0);
-            notas.Add(5);
-            notas.Add(9);
             notas.Add(3);
-            notas.Add(7);
             notas.Add(4);
+            notas.Add(5);
+            notas.Add(7);
             notas.Add(8);
+            notas.Add(9);
 
+            // Resultados esperados
             double mediaEsperada = 5.143;
             int susE = 3;
             int aprE = 1;
             int notE = 2;
             int sbrE = 1;
+
+            // Ejecución del caso
+            EstadisticasNotas estadistica = new EstadisticasNotas();
+            double mediaActual = estadistica.CalcularEstadisticas(notas);
+
+            // Comprobación
+            Assert.AreEqual(mediaEsperada, mediaActual, 0.001, "Medias distintas. Esperada: " + mediaEsperada + ". Actual: " + mediaActual);
+            Assert.AreEqual(estadistica.Suspensos, susE, 0, "No coincide el número de suspensos.");
+            Assert.AreEqual(estadistica.Aprobados, aprE, 0, "No coincide el número de aprobados.");
+            Assert.AreEqual(estadistica.Notables, notE, 0, "No coincide el número de notables.");
+            Assert.AreEqual(estadistica.Sobresalientes, sbrE, 0, "No coincide el número de sobresalientes.");
+        }
+
+        [TestMethod]
+        public void CalcularEstadisticas_ListaSinValores_ProvocaExcepcion()
+        {
+            // Preparación de la prueba
+            List<int> notas = new List<int>();
+
+            // Ejecución del caso
+            try
+            {
+                EstadisticasNotas estadistica = new EstadisticasNotas();
+                double mediaActual = estadistica.CalcularEstadisticas(notas);
+            }
+            catch (Exception exception)
+            {
+                StringAssert.Contains(exception.Message, "Lista vacia");
+                return;
+            }
+
+            Assert.Fail("No ha devuelto excepción de lista vacía");
+        }
+
+        [TestMethod]
+        public void CalcularEstadisticas_NotaNegativa_ProvocaExcepcion()
+        {
+            // Preparación de la prueba
+            List<int> notas = new List<int>();
+            notas.Add(0);
+            notas.Add(3);
+            notas.Add(4);
+            notas.Add(5);
+            notas.Add(-7);
+
+            // Ejecución del caso
+            try
+            {
+                EstadisticasNotas estadistica = new EstadisticasNotas();
+                double mediaActual = estadistica.CalcularEstadisticas(notas);
+            }
+            catch (Exception exception)
+            {
+                StringAssert.Contains(exception.Message, "Nota invalida");
+                return;
+            }
+
+            Assert.Fail("No ha devuelto excepción de lista vacía");
+        }
+
+        [TestMethod]
+        public void CalcularEstadisticas_NotaSuperiorDiez_ProvocaExcepcion()
+        {
+            // Preparación de la prueba
+            List<int> notas = new List<int>();
+            notas.Add(4);
+            notas.Add(5);
+            notas.Add(12);
+
+            // Ejecución del caso
+            try
+            {
+                EstadisticasNotas estadistica = new EstadisticasNotas();
+                double mediaActual = estadistica.CalcularEstadisticas(notas);
+            }
+            catch (Exception exception)
+            {
+                StringAssert.Contains(exception.Message, "Nota invalida");
+                return;
+            }
+
+            Assert.Fail("No ha devuelto excepción de lista vacía");
+        }
+
+        [TestMethod]
+        public void CalcularEstadisticas_ListaUnicoElemento_ActualizaNotas()
+        {
+            // Preparación de la prueba
+            List<int> notas = new List<int>();
+            notas.Add(6);
+
+            // Resultados esperados
+            double mediaEsperada = 6;
+            int susE = 0;
+            int aprE = 1;
+            int notE = 0;
+            int sbrE = 0;
+
+            // Ejecución del caso
+            EstadisticasNotas estadistica = new EstadisticasNotas();
+            double mediaActual = estadistica.CalcularEstadisticas(notas);
+
+            // Comprobación
+            Assert.AreEqual(mediaEsperada, mediaActual, 0.001, "Medias distintas. Esperada: " + mediaEsperada + ". Actual: " + mediaActual);
+            Assert.AreEqual(estadistica.Suspensos, susE, 0, "No coincide el número de suspensos.");
+            Assert.AreEqual(estadistica.Aprobados, aprE, 0, "No coincide el número de aprobados.");
+            Assert.AreEqual(estadistica.Notables, notE, 0, "No coincide el número de notables.");
+            Assert.AreEqual(estadistica.Sobresalientes, sbrE, 0, "No coincide el número de sobresalientes.");
+        }
+
+        [TestMethod]
+        public void CalcularEstadisticas_NotaCero_ActualizaNotas()
+        {
+            // Preparación de la prueba
+            List<int> notas = new List<int>();
+            notas.Add(0);
+
+            // Resultados esperados
+            double mediaEsperada = 0;
+            int susE = 1;
+            int aprE = 0;
+            int notE = 0;
+            int sbrE = 0;
+
+            // Ejecución del caso
+            EstadisticasNotas estadistica = new EstadisticasNotas();
+            double mediaActual = estadistica.CalcularEstadisticas(notas);
+
+            // Comprobación
+            Assert.AreEqual(mediaEsperada, mediaActual, 0.001, "Medias distintas. Esperada: " + mediaEsperada + ". Actual: " + mediaActual);
+            Assert.AreEqual(estadistica.Suspensos, susE, 0, "No coincide el número de suspensos.");
+            Assert.AreEqual(estadistica.Aprobados, aprE, 0, "No coincide el número de aprobados.");
+            Assert.AreEqual(estadistica.Notables, notE, 0, "No coincide el número de notables.");
+            Assert.AreEqual(estadistica.Sobresalientes, sbrE, 0, "No coincide el número de sobresalientes.");
+        }
+
+        [TestMethod]
+        public void CalcularEstadisticas_NotaDiez_ActualizaNotas()
+        {
+            // Preparación de la prueba
+            List<int> notas = new List<int>();
+            notas.Add(10);
+
+            // Resultados esperados
+            double mediaEsperada = 10;
+            int susE = 0;
+            int aprE = 0;
+            int notE = 0;
+            int sbrE = 1;
+
+            // Ejecución del caso
+            EstadisticasNotas estadistica = new EstadisticasNotas();
+            double mediaActual = estadistica.CalcularEstadisticas(notas);
+
+            // Comprobación
+            Assert.AreEqual(mediaEsperada, mediaActual, 0.001, "Medias distintas. Esperada: " + mediaEsperada + ". Actual: " + mediaActual);
+            Assert.AreEqual(estadistica.Suspensos, susE, 0, "No coincide el número de suspensos.");
+            Assert.AreEqual(estadistica.Aprobados, aprE, 0, "No coincide el número de aprobados.");
+            Assert.AreEqual(estadistica.Notables, notE, 0, "No coincide el número de notables.");
+            Assert.AreEqual(estadistica.Sobresalientes, sbrE, 0, "No coincide el número de sobresalientes.");
         }
     }
 }
